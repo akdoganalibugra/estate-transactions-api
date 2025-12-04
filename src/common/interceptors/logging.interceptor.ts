6bc487eba0c14d7416d0db85b3bc12ee29938432
Ali Bugra <akdoganalibugra@gmail.com>
2025-12-04 20:54:33 +0300

    feat: add type annotations for request and response in LoggingInterceptor

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -1,19 +1,20 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
     private readonly logger = new Logger(LoggingInterceptor.name);
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        const request = context.switchToHttp().getRequest();
+        const request = context.switchToHttp().getRequest<Request>();
         const { method, url } = request;
         const now = Date.now();
 
         return next.handle().pipe(
             tap(() => {
-                const response = context.switchToHttp().getResponse();
+                const response = context.switchToHttp().getResponse<Response>();
                 const { statusCode } = response;
                 const delay = Date.now() - now;
 
